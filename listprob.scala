479f3ba2613d070c2910fe6b212cbf622c2d1cdc
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-20 09:32:41 +0530

    Define compress function on list of symbols

--- listprob.scala
+++ listprob.scala
@@ -6,9 +6,15 @@ object ListManip{
   def penultimate(list : List[Int]) : Int =  {
     list(list.length - 2)
   }
+
+  def compress(list: List[Symbol]) : List[Symbol] = {
+   if (list.size == 0) list
+   else List(list(0)) ::: compress(list.filter((x) => x != list(0)))
+  }
   
   def main(args: Array[String]) = {
     assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
     assert( penultimate(List(1, 1, 2, 3, 5, 8)) == 5 )
+    assert( compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) == List('a, 'b, 'c, 'd, 'e) )
   }
 }
