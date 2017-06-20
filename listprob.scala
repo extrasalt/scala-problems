c2953a8ba1b95748bf32f7e90f4f33d73091147b
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-20 10:18:27 +0530

    Add reverse function

--- listprob.scala
+++ listprob.scala
@@ -30,6 +30,14 @@ object ListManip{
     val (left, right) = list.splitAt(r)
     left.drop(l)
   }
+
+  def reverse(list : List[Int]) : List[Int] = {
+    if(list.size == 1) list
+    else {
+     val (left, right) = list.splitAt(list.length-1)
+     right ::: reverse(left)
+    }
+  }
   
   def main(args: Array[String]) = {
     assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
@@ -38,6 +46,7 @@ object ListManip{
     assert(sum3sAnd5s(1 to 9) == 23)
     assert(drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) == List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
     assert(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)).equals(List('d, 'e, 'f, 'g)))
+    assert(reverse(List(1, 1, 2, 3, 5, 8)).equals(List(8, 5, 3, 2, 1, 1)))
     println(sum3sAnd5s(1 to 999))
   }
 }
