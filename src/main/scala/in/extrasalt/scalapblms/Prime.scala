5c014aab98df99a4cfa870b36774f5a62298417f
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-21 09:06:00 +0530

    Change logic to find prime

--- src/main/scala/in/extrasalt/scalapblms/Prime.scala
+++ src/main/scala/in/extrasalt/scalapblms/Prime.scala
@@ -5,8 +5,8 @@ object Prime {
   }
 
   def isPrime(n: Int) = {
-    val s = (2 to n / 2).toList.map((x) => n % x).sum
-    s == 0
+    val s = (2 to n / 2).toList.map((x) => n % x).contains(0)
+    !s
   }
 
   def listPrimesinRange(range: Range): List[Int] = {
