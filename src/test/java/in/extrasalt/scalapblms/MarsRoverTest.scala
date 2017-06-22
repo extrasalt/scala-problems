3aacbc5b2a859e5d57d11f64b65f18529c10e475
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-22 15:24:19 +0530

    Test if Rover can handle input as string

--- src/test/java/in/extrasalt/scalapblms/MarsRoverTest.scala
+++ src/test/java/in/extrasalt/scalapblms/MarsRoverTest.scala
@@ -9,19 +9,22 @@ class RoverTest extends FlatSpec {
     new Rover(0, 0, "E").position() should be("0 0 E")
   }
 
-  "Rover" should "move forward" in {
+  it should "move forward" in {
     new Rover(0,0, "E").moveForward().position() should be("1 0 E")
     new Rover(2,2, "W").moveForward().position() should be("1 2 W")
     new Rover(2,2, "N").moveForward().position() should be("2 3 N")
     new Rover(2,2, "S").moveForward().position() should be("2 1 S")
   }
 
-  "Rover" should "rotate left" in {
+  it should "rotate left" in {
     new Rover(0, 0, "E").rotateLeft().position() should be("0 0 N")
   }
 
-  "Rover" should "rotate right" in {
+  it should "rotate right" in {
     new Rover(0, 0, "E").rotateRight().position() should be("0 0 S")
   }
 
+  it should "handle input in the form of a string" in {
+    new Rover(0, 0, "E").handleInputString("MMLM").position() should be("2 1 N")
+  }
 }
