ec69f9699df55f93c5a191d6a1b9b60e344f0bdf
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-23 10:43:29 +0530

    Refactor Rover test and Fix failing test

---
+++ src/test/scala/in/extrasalt/scalapblms/RoverTest.scala
@@ -0,0 +1,30 @@
+package in.extrasalt.scalapblms
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers.{be, convertToAnyShouldWrapper}
+
+class RoverTest extends FlatSpec {
+
+  "Rover" should "print position" in {
+    Rover(0, 0, "E").position() should be("0 0 E")
+  }
+
+  it should "move forward" in {
+    Rover(0,0, "E").moveForward().position() should be("1 0 E")
+    Rover(2,2, "W").moveForward().position() should be("1 2 W")
+    Rover(2,2, "N").moveForward().position() should be("2 3 N")
+    Rover(2,2, "S").moveForward().position() should be("2 1 S")
+  }
+
+  it should "rotate left" in {
+    Rover(0, 0, "E").rotateLeft().position() should be("0 0 N")
+  }
+
+  it should "rotate right" in {
+    Rover(0, 0, "E").rotateRight().position() should be("0 0 S")
+  }
+
+  it should "handle input in the form of a string" in {
+    Rover(0, 0, "E").handleInputString("MMLM").position() should be("2 1 N")
+  }
+}
