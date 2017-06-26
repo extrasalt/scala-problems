e7ccddbf2bd0eea213c7e7feb7e82888b82456f0
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-26 16:36:19 +0530

    Add countHop and Augment string helpers

--- src/main/scala/in/extrasalt/scalapblms/WordChain.scala
+++ src/main/scala/in/extrasalt/scalapblms/WordChain.scala
@@ -1,12 +1,39 @@
 package in.extrasalt.scalapblms
 
-class WordChain(start: String, end: String) {
+import scala.io.Source
 
-  def chainExists(): Boolean = {
-    if (start.size != end.size) {
-      return false
+class WordChain(start: String, end: String, dictionary: List[String]) {
+
+  val filterdDictionary = dictionary.filter(_.length == start.length)
+
+  def chainExists(): Boolean = start.length == end.length
+
+}
+
+object WordChain {
+
+  def readDictionary() = {
+    Source.fromFile("/words.dat").getLines.toList
+  }
+
+  def countHopAway(a: String, b: String, i: Int = 0, count: Int = 0): Int = {
+
+    if (i == a.length) { return count }
+    val augmentedString = augmentString(b, a, i)
+    if (augmentedString == b) countHopAway(a, augmentedString, i + 1, count)
+    else countHopAway(a, augmentedString, i + 1, count + 1)
+  }
+
+  def augmentString(s: String, from: String, at: Int): String = {
+    val aList = from.toList
+    val bList = s.toList
+
+    val augmentedB = bList.zipWithIndex.map{
+      case (c, i) => if (i == at) aList(at) else c
     }
-    return true
+
+    augmentedB.mkString
+
   }
 
 }
--- src/test/scala/in/extrasalt/scalapblms/WordChainTest.scala
+++ src/test/scala/in/extrasalt/scalapblms/WordChainTest.scala
@@ -1,10 +1,20 @@
 package in.extrasalt.scalapblms
 
 import org.scalatest.FlatSpec
-import org.scalatest.Matchers.{be, convertToAnyShouldWrapper}
+import org.scalatest.Matchers._
 
 class WordChainTest extends FlatSpec {
   "WordChain" should "return false if the size of inputs don't match" in {
-    new WordChain("yellow", "hello").chainExists() should be(false)
+    new WordChain("yellow", "hello", List("card", "tomato")).chainExists() should be(false)
   }
+
+  "WordChain" should "return correct number of hops away" in {
+    WordChain.countHopAway("yellow", "hellow") should be(1)
+    WordChain.countHopAway("too", "tre") should be(2)
+  }
+
+  "WordChain" should "be able to augment a string with another string" in {
+    WordChain.augmentString("Hello", "Tryan", 2) should be("Heylo")
+  }
+
 }
