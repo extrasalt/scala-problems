e7ebd300fc5a1330682f4814d86ab4a4b8ffbfa6
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-06-27 10:31:21 +0530

    Add Scrable getWordValue method

---
+++ src/test/scala/in/extrasalt/scalapblms/ScrabbleProblemTest.scala
@@ -0,0 +1,41 @@
+package in.extrasalt.scalapblms
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+
+class ScrabbleProblemTest extends FlatSpec {
+
+    "Scrabble" should "return the value of the given string" in {
+      val scoreMap: Map[Char, Int] = Map(
+        'a' -> 1,
+        'b' -> 3,
+        'c' -> 3,
+        'd' -> 2,
+        'e' -> 1,
+        'f' -> 4,
+        'g' -> 2,
+        'h' -> 4,
+        'i' -> 1,
+        'j' -> 8,
+        'k' -> 5,
+        'l' -> 1,
+        'm' -> 3,
+        'n' -> 1,
+        'o' -> 1,
+        'p' -> 3,
+        'q' -> 10,
+        'r' -> 1,
+        's' -> 1,
+        't' -> 1,
+        'u' -> 1,
+        'v' -> 4,
+        'w' -> 4,
+        'x' -> 8,
+        'y' -> 4,
+        'z' -> 10
+      )
+      
+      ScrabbleProblem.getWordValue("indix", scoreMap) should be(13)
+    }
+
+}
