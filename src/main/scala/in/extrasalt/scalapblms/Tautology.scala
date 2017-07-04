174e1fc28dcef281c7541004fb632bfc3c8f7cf1
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-07-04 11:55:30 +0530

    Add function to evaluatePostfix

--- src/main/scala/in/extrasalt/scalapblms/Tautology.scala
+++ src/main/scala/in/extrasalt/scalapblms/Tautology.scala
@@ -34,6 +34,40 @@ object Tautology {
     eval(stack = List(), list, postfix = List()).mkString("")
   }
 
+  def handleOp(operator: Char,
+               stack: List[Char],
+               tail: List[Char],
+               eval: (List[Char], List[Char]) => List[Char]): List[Char] = {
+    val a        = stack.tail.head
+    val b        = stack.head
+    val newStack = stack.tail.tail
+
+    val opresult = operator match {
+      case '&' => '1'
+      case '|' => '1'
+    }
+    //TODO: Evaluate a op b
+
+    eval(newStack, opresult :: tail)
+  }
+
+  def evaluatePostfix(expression: String): String = {
+    val list = expression.toList
+
+    def eval(stack: List[Char], postfix: List[Char]): List[Char] = {
+      postfix.head match {
+        case 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' =>
+          eval(postfix.head :: stack, postfix.tail)
+        case '&' => handleOp('&', stack, postfix.tail, eval)
+        case '|' => handleOp('|', stack, postfix.tail, eval)
+
+      }
+    }
+
+    eval(stack = List(), list).mkString("")
+
+  }
+
   def isVariableCountBalanced(expression: String): Boolean =
     !expression
       .replaceAll("[&|\\|\\(\\)\\!]", "")
