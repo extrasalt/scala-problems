67bae05a2bea818ec10e6f081dbc98ae63d0e735
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-07-06 16:51:31 +0530

    Add Convert func to parse roman numbers to hindu numbers

--- src/main/scala/in/extrasalt/scalapblms/CurrencyExchange.scala
+++ src/main/scala/in/extrasalt/scalapblms/CurrencyExchange.scala
@@ -7,3 +7,32 @@ class CurrencyExchange(names: Map[String, String]) {
   }
 
 }
+
+object CurrencyExchange {
+  def convert(roman : String): Int = {
+//    "I" can be subtracted from "V" and "X" only.
+//    "X" can be subtracted from "L" and "C" only.
+//    "C" can be subtracted from "D" and "M" only.
+
+    def loop(roman: List[Char], acc: Int): Int = roman match {
+      case Nil => acc
+      case a::b::tail if a=='I' && b =='V' => loop(tail,acc+4)
+      case a::b::tail if a=='I' && b == 'X' => loop(tail, acc+9)
+      case a::b::tail if a=='X' && b == 'L' => loop(tail, acc+40)
+      case a::b::tail if a=='X' && b == 'C' => loop(tail, acc+90)
+      case a::b::tail if a=='C' && b == 'D' => loop(tail, acc+400)
+      case a::b::tail if a=='C' && b == 'M' => loop(tail, acc+900)
+      case a::tail if a=='I' => loop(tail,acc+1)
+      case a::tail if a=='V' => loop(tail,acc+5)
+      case a::tail if a=='X' => loop(tail,acc+10)
+      case a::tail if a=='L' => loop(tail,acc+50)
+      case a::tail if a=='C' => loop(tail,acc+100)
+      case a::tail if a=='D' => loop(tail,acc+500)
+      case a::tail if a=='M' => loop(tail,acc+1000)
+
+    }
+
+    loop(roman.toList, 0)
+
+  }
+}
--- src/test/scala/in/extrasalt/scalapblms/CurrencyExchangeTest.scala
+++ src/test/scala/in/extrasalt/scalapblms/CurrencyExchangeTest.scala
@@ -1,11 +1,13 @@
 package in.extrasalt.scalapblms
 
 import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+
 
 class CurrencyExchangeTest extends FlatSpec {
 
-  "CurrencyExchange" should "return roman characters for given question" in {
-    new CurrencyExchange(Map("glob" -> "I", "prok" -> "V"))
+  "CurrencyExchange" should "convert roman numerals into actual numbers" in {
+    CurrencyExchange.convert("MCMIII") should be(1903)
   }
 
 }
