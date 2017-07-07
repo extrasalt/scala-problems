6720fcc64f171022238a76d9bd54fc4ce152ed9a
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-07-07 14:14:42 +0530

    Hardcode scrabble values

--- src/main/scala/in/extrasalt/scalapblms/ScrabbleProblem.scala
+++ src/main/scala/in/extrasalt/scalapblms/ScrabbleProblem.scala
@@ -30,39 +30,36 @@ object ScrabbleProblem {
     'z' -> 10
   )
 
-  def getWordValue(string: String, scoreMap: Map[Char, Int]) = {
+
+  def modify(string: String, scoreMap: Map[Char, Int]): Map[Char, Int] = {
+
+    def loop(string: String, scoreMap: Map[Char, Int]): Map[Char, Int] ={
+      loop(string.tail, scoreMap.map{case (c,i)=>(c,i*2)})
+    }
+
+    scoreMap
+  }
+
+  def getWordValue(string: String, scoreMap: Map[Char, Int]): Int = {
     string.toList.map((x) => scoreMap(x)).sum
   }
 
-  val scrabbleBoard: Map[(Int, Int), String] = {
-    val matrix = (0 to 14).toList.flatMap((x) => (0 to 14).toList.map((y) => (x, y)))
-    val dlSet = {
-      matrix.filter((t) =>
-        (t._1, t._2) match {
-          case (x, y) if (x == 6 || x == 8) && (y == 6 || y == 8) => true
-          case (3, 7) | (7, 3) | (11, 7) | (7, 11)                => true
-          case (x, y) if (x % 8 == 3) && (y % 7 == 0) => true
-          case (x, y) if (y % 8 == 3) && (x % 7 == 0) => true
-          case (x, y) if Set(2, 6, 8, 12).contains(x) && Set(2, 6, 8, 12).contains(y) && x != y => true
-          case _                                                                                => false
-      })
-    }
-
-    val modifiers = matrix.map((t) =>
-      (t._1, t._2) match {
-        case (7, 7) => "*"
-        case (x, y) if x % 7 == 0 && y % 7 == 0 => "triple word"
-        case (x, y) if x % 4 == 1 && y % 4 == 1 &&  (x != y) => "triple letter"
-        case (x, y) if (x == 9 || x ==5) && (y == 9 || y == 5) => "triple letter"
-        case (x, y) if (x % 9 < 5) && (y % 9 <5) && (x ==y || x == 14-y)  => "double word"
-
-        case (x, y) if dlSet.contains((x, y)) => "double letter"
-
-        case _                                => "normal"
-    })
-
-    matrix.zip(modifiers).toMap
-
-  }
+  val scrabbleBoard: List[List[String]] = List(
+    List("TW", "NL", "NL", "DL", "NL", "NL", "NL", "TW", "NL", "NL", "NL", "DL", "NL", "NL", "TW"),
+    List("NL", "DW", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "DW", "NL"),
+    List("NL", "NL", "DW", "NL", "NL", "NL", "DL", "NL", "DL", "NL", "NL", "NL", "DW", "NL", "NL"),
+    List("DL", "NL", "NL", "DW", "NL", "NL", "NL", "DL", "NL", "NL", "NL", "DW", "NL", "NL", "DL"),
+    List("NL", "NL", "NL", "NL", "DW", "NL", "NL", "NL", "NL", "NL", "DW", "NL", "NL", "NL", "NL"),
+    List("NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL"),
+    List("NL", "NL", "DL", "NL", "NL", "NL", "DL", "NL", "DL", "NL", "NL", "NL", "DL", "NL", "NL"),
+    List("TW", "NL", "NL", "DL", "NL", "NL", "NL", "NL", "NL", "NL", "NL", "DL", "NL", "NL", "TW"),
+    List("NL", "NL", "DL", "NL", "NL", "NL", "DL", "NL", "DL", "NL", "NL", "NL", "DL", "NL", "NL"),
+    List("NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL"),
+    List("NL", "NL", "NL", "NL", "DW", "NL", "NL", "NL", "NL", "NL", "DW", "NL", "NL", "NL", "NL"),
+    List("DL", "NL", "NL", "DW", "NL", "NL", "NL", "DL", "NL", "NL", "NL", "DW", "NL", "NL", "DL"),
+    List("NL", "NL", "DW", "NL", "NL", "NL", "DL", "NL", "DL", "NL", "NL", "NL", "DW", "NL", "NL"),
+    List("NL", "DW", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "TL", "NL", "NL", "NL", "DW", "NL"),
+    List("TW", "NL", "NL", "DL", "NL", "NL", "NL", "TW", "NL", "NL", "NL", "DL", "NL", "NL", "TW")
+  )
 
 }
--- src/test/scala/in/extrasalt/scalapblms/ScrabbleProblemTest.scala
+++ src/test/scala/in/extrasalt/scalapblms/ScrabbleProblemTest.scala
@@ -5,44 +5,17 @@ import org.scalatest.Matchers._
 
 class ScrabbleProblemTest extends FlatSpec {
 
-    "Scrabble" should "return the value of the given string" in {
-      val scoreMap: Map[Char, Int] = Map(
-        'a' -> 1,
-        'b' -> 3,
-        'c' -> 3,
-        'd' -> 2,
-        'e' -> 1,
-        'f' -> 4,
-        'g' -> 2,
-        'h' -> 4,
-        'i' -> 1,
-        'j' -> 8,
-        'k' -> 5,
-        'l' -> 1,
-        'm' -> 3,
-        'n' -> 1,
-        'o' -> 1,
-        'p' -> 3,
-        'q' -> 10,
-        'r' -> 1,
-        's' -> 1,
-        't' -> 1,
-        'u' -> 1,
-        'v' -> 4,
-        'w' -> 4,
-        'x' -> 8,
-        'y' -> 4,
-        'z' -> 10
-      )
-      
-      ScrabbleProblem.getWordValue("indix", scoreMap) should be(13)
+    "Scrabble board" should "have a proper count of all modifiers" in {
+      ScrabbleProblem.scrabbleBoard.flatten.count((x) => x == "TW") should be(8)
+      ScrabbleProblem.scrabbleBoard.flatten.count((x) => x == "DL") should be(24)
+      ScrabbleProblem.scrabbleBoard.flatten.count((x) => x == "TL") should be(12)
+      ScrabbleProblem.scrabbleBoard.flatten.count((x) => x == "DW") should be(16)
+
+
     }
 
-    "Scrabble board" should "have a proper count of all modifiers" in {
-      ScrabbleProblem.scrabbleBoard.values.count((x) => x == "triple word") should be(8)
-      ScrabbleProblem.scrabbleBoard.values.count((x) => x == "triple letter") should be(12)
-      ScrabbleProblem.scrabbleBoard.values.count((x) => x == "double word") should be(16)
-      ScrabbleProblem.scrabbleBoard.values.count((x) => x == "double letter") should be(24)
+    it should "return a word value for string starting at 0, 0 R" in {
+      ScrabbleProblem.getWordValue("indix", ScrabbleProblem.defaultScoreMap) should be(13)
 
 
     }
